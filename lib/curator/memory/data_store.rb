dc18eda74a830586186c0b106073cfd964dc9987
John Downey <jdowney@gmail.com>
2012-02-28 21:02:56 -0600

    Small formatting changes

--- lib/curator/memory/data_store.rb
+++ lib/curator/memory/data_store.rb
@@ -1,14 +1,16 @@
 require 'ostruct'
 
-module Curator::Memory
-  class DataStore
-    class << self
-      def remove_all_keys
+module Curator
+  module Memory
+    class DataStore
+      def self.remove_all_keys
         @data = {}
       end
-      alias :reset! :remove_all_keys
+      class << self
+        alias :reset! :remove_all_keys
+      end
 
-      def save(options)
+      def self.save(options)
         bucket = _bucket_name(options[:collection_name])
         object = options[:value]
         key = options[:key]
@@ -26,7 +28,7 @@ module Curator::Memory
         OpenStruct.new(:key => key)
       end
 
-      def delete(collection_name, key)
+      def self.delete(collection_name, key)
         bucket = _bucket_name(collection_name)
         _records(bucket).delete(key)
         _indices(bucket).each_key do |name|
@@ -39,67 +41,67 @@ module Curator::Memory
         end
       end
 
-      def find_by_key(collection_name, key)
+      def self.find_by_key(collection_name, key)
         bucket = _bucket_name(collection_name)
         value = _records(bucket).fetch(key, nil)
         return if value.nil?
         {:key => key, :data => value}
       end
 
-      def find_by_index(collection_name, index_name, query)
+      def self.find_by_index(collection_name, index_name, query)
         return [] if query.nil?
         bucket = _bucket_name(collection_name)
         index = _index(bucket, index_name)
         keys = case query
-        when Range
-          first = _convert_for_query(query.first)
-          last = _convert_for_query(query.last)
-          keys = index.keys.select do |key|
-            key = _convert_for_query(key)
-            key.between?(first, last)
-          end
-          index.values_at(*keys).flatten
-        else
-          index.fetch(query, [])
-        end
+               when Range
+                 first = _convert_for_query(query.first)
+                 last = _convert_for_query(query.last)
+                 keys = index.keys.select do |key|
+                   key = _convert_for_query(key)
+                   key.between?(first, last)
+                 end
+                 index.values_at(*keys).flatten
+               else
+                 index.fetch(query, [])
+               end
         keys.map do |key|
           find_by_key(collection_name, key)
         end
       end
 
-      def _convert_for_query(value)
+      def self._convert_for_query(value)
         Time.parse(value.to_s)
       rescue ArgumentError
         value
       end
 
-      def _data
+      def self._data
         @data ||= {}
       end
 
-      def _bucket(bucket)
+      def self._bucket(bucket)
         _data[bucket] ||= {}
       end
 
-      def _records(bucket)
+      def self._records(bucket)
         _bucket(bucket)[:records] ||= {}
       end
 
-      def _indices(bucket)
+      def self._indices(bucket)
         _bucket(bucket)[:indices] ||= {}
       end
 
-      def _index(bucket, index_name)
+      def self._index(bucket, index_name)
         _indices(bucket)[index_name] ||= {}
       end
 
-      def _generate_key(bucket)
+      def self._generate_key(bucket)
         keys = _records(bucket).keys
         keys = [0] if keys.empty?
         keys.max.next
       end
 
-      def _bucket_name(name)
+      def self._bucket_name(name)
         "#{Curator.config.environment}:#{name}"
       end
     end
