73639e17a1df2e6a34c2d644fd910fc16b6d1762
Matthew M. Nelson <dogenpunk@gmail.com>
2012-03-05 17:07:58 -0600

    1.8 hash syntax; Use def self.method_name

--- spec/curator/mongo/data_store_spec.rb
+++ spec/curator/mongo/data_store_spec.rb
@@ -35,46 +35,46 @@ module Curator
 
       describe "self.save" do
         it "namespaces collections with app and environment" do
-          DataStore.save(collection_name: 'fake_things', key: 1, value: {foo: "bar"})
+          DataStore.save(:collection_name => 'fake_things', :key => 1, :value => {:foo => "bar"})
           DataStore.client.db(DataStore._db_name).collection('fake_things').find_one({'_id' => 1}).should == {"_id" => 1, "foo" => "bar"}
         end
 
         it 'updates objects in place' do
-          DataStore.save(collection_name: 'fake_things', key: 'foo', value: {'foo' => 1})
-          DataStore.save(collection_name: 'fake_things', key: 'foo', value: {'foo' => 2})
-          DataStore.find_by_key('fake_things', 'foo').should == {key: 'foo', data: {'foo' => 2}}
+          DataStore.save(collection_name: 'fake_things', :key => 'foo', :value => {'foo' => 1})
+          DataStore.save(collection_name: 'fake_things', :key => 'foo', :value => {'foo' => 2})
+          DataStore.find_by_key('fake_things', 'foo').should == {:key => 'foo', :data => {'foo' => 2}}
         end
 
         it 'generates a new key if not provided' do
-          DataStore.save(collection_name: 'fake_things', key: 'goonie', value: {foo: 1})
-          id = DataStore.save(collection_name: 'fake_things',  value: {foo: 2})
+          DataStore.save(:collection_name => 'fake_things', :key => 'goonie', :value => {:foo => 1})
+          id = DataStore.save(:collection_name => 'fake_things', :value => {:foo => 2})
           DataStore.find_by_key('fake_things', id[:key]).should == {:key => id[:key], :data => {'foo' => 2}}
         end
         
         it 'returns an object with a key' do
-          object = DataStore.save(collection_name: 'fake_things', key: 'abc', value: {foo: 1})
+          object = DataStore.save(:collection_name => 'fake_things', :key => 'abc', :value => {:foo => 1})
           object.should respond_to :key
         end
 
         it "can index by multiple things" do
           begin
             DataStore.save(
-                           collection_name: 'fake_things',
-                           key: 'blah', value: {foo: 'foo-data', bar: 'bar-data'},
-                           index: {foo: 1, bar: 1})
+                           :collection_name => 'fake_things',
+                           :key => 'blah', :value => {:foo => 'foo-data', :bar => 'bar-data'},
+                           :index => {:foo => 1, :bar => 1})
             foo_result = DataStore.find_by_index("fake_things", "foo", "foo-data").first
             foo_result[:key].should == 'blah'
             bar_result = DataStore.find_by_index("fake_things", "bar", "bar-data").first
             bar_result[:key].should == 'blah'
           ensure
-            DataStore._collection("fake_things").remove(_id: "blah")
+            DataStore._collection("fake_things").remove(:_id => "blah")
           end
         end
       end
 
       describe "self.delete" do
         it "deletes an object in a collection for a key" do
-          DataStore.save(collection_name: 'heap', key: 10, value: {k: "v"})
+          DataStore.save(:collection_name => 'heap', :key => 10, :value => {:k => "v"})
           DataStore.delete("heap", 10)
           DataStore.find_by_key("heap", 10).should be_nil
         end
@@ -90,8 +90,8 @@ module Curator
         end
 
         it "returns multiple objects" do
-          DataStore.save(collection_name: 'test_collection', key: 10, value: {indexed_key: "indexed_value"}, index: {indexed_key: 'indexed_value'})
-          DataStore.save(collection_name: 'test_collection', key: 11, value: {indexed_key: "indexed_value"}, index: {indexed_key: 'indexed_value'})
+          DataStore.save(:collection_name => 'test_collection', :key => 10, :value => {:indexed_key => "indexed_value"}, :index => {:indexed_key => 'indexed_value'})
+          DataStore.save(:collection_name => 'test_collection', :key => 11, :value => {:indexed_key => "indexed_value"}, :index => {:indexed_key => 'indexed_value'})
           keys = DataStore.find_by_index('test_collection', :indexed_key, 'indexed_value').map {|data| data[:key] }
           keys.map(&:to_s).sort.should == ['10', '11']
         end
@@ -99,7 +99,7 @@ module Curator
 
       describe "find_by_key" do
         it "returns an object by key" do
-          DataStore.save(collection_name: "heap", key: 10, value: {k: "v"})
+          DataStore.save(:collection_name => "heap", :key => 10, :value => {:k => "v"})
           DataStore.find_by_key("heap", 10).should == {:key => 10, :data => {'k' => 'v'}}
         end
 
